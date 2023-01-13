f020ca2fe5da2665277dcda91fe1b6909f0129ca
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-13 13:07:11 +0700

    java generic Invariant

---
+++ src/ardhastudio/Application/InvariantApp.java
@@ -0,0 +1,9 @@
+package ardhastudio.Application;
+
+import ardhastudio.MyData;
+
+public class InvariantApp {
+    public static void main(String[] args) {
+        MyData<String> stringMyData = new MyData<String>("Kawasaki");
+    }
+}
